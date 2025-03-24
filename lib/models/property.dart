f5e700ec36403191d2017c03e0e974a38a538095
Enami5343 <enami15-5343@diu.edu.bd>
2025-03-24 20:33:08 +0600

    Create property.dart

---
+++ lib/models/property.dart
@@ -0,0 +1,27 @@
+class Property {
+  final String imageUrl;
+  final String location;
+  final String price;
+  final String availability;
+  final int bedroom;
+  final int bathroom;
+  final int balcony;
+  final int kitchen;
+  final String userName;
+  final String userImage;
+  final DateTime postTime;
+
+  Property({
+    required this.imageUrl,
+    required this.location,
+    required this.price,
+    required this.availability,
+    required this.bedroom,
+    required this.bathroom,
+    required this.balcony,
+    required this.kitchen,
+    required this.userName,
+    required this.userImage,
+    required this.postTime,
+  });
+}
