3f95cd58e03438067ef402cc709011e4cc1c8423
Ashibur Rahman <rahman15-5137@diu.edu.bd>
2025-03-25 20:45:37 +0600

    Revert "Update main.dart"
    This reverts commit e756db3cd7efbe9f201eca0dab2d977e420eeb40.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
-import 'providers/favorites_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => FavoritesProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: LoginScreen(), // Start with the Login Screen
     );
   }
 }
